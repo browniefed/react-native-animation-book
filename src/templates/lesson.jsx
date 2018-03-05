3b7578b6f82209977175f9478215dc158e816b65
Jason Brown <browniefed@gmail.com>
2018-03-04 19:33:15 -0800

    Fix up header to push towards rn animations

--- src/templates/lesson.jsx
+++ src/templates/lesson.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Helmet from "react-helmet";
 import styled from "react-emotion";
-
-import SEO from "../components/SEO/SEO"
-import SiteHeader from '../components/Layout/Header'
-import config from "../../data/SiteConfig"
+import { Follow } from "react-twitter-widgets";
+import SEO from "../components/SEO/SEO";
+import SiteHeader from "../components/Layout/Header";
+import config from "../../data/SiteConfig";
 import TableOfContents from "../components/Layout/TableOfContents";
 
 export default class LessonTemplate extends React.Component {
@@ -37,6 +37,51 @@ export default class LessonTemplate extends React.Component {
           </ToCContainer>
           <BodyContainer>
             <div>
+              <div>
+                <a
+                  style={{
+                    marginTop: "40px",
+                    width: "100%",
+                    display: "flex",
+                    justifyContent: "flex-end",
+                    alignItems: "flex-start",
+                    border: "1px solid #DDD",
+                    textDecoration: "none",
+                    minHeight: "150px",
+                  }}
+                  href="https://reactnativeanimations.com"
+                  target="_blank"
+                >
+                  <img
+                    style={{ height: "150px" }}
+                    src="https://images.codedaily.io/courses/react_native_animations.png"
+                    className
+                  />
+                  <div style={{ float: "left", marginLeft: 15 }}>
+                    <h2
+                      style={{
+                        margin: 0,
+                        color: "#333",
+                        lineHeight: 1,
+                        marginTop: 5,
+                        marginBottom: 5,
+                      }}
+                    >
+                      Master React Native Animations Course
+                    </h2>
+                    <p style={{ color: "#666", margin: 0 }}>
+                      Go from beginner to mastery of React Native animations. We'll walk you through
+                      each step from animating a simple box, understanding interpolation, all the
+                      way up to building a shared element photo grid! Get it at
+                      https://reactnativeanimations.com
+                    </p>
+                  </div>
+                </a>
+                <div style={{ marginTop: 15 }}>
+                  <Follow username="browniefed" />
+                  <Follow username="codedailyio" />
+                </div>
+              </div>
               <div dangerouslySetInnerHTML={{ __html: postNode.html }} />
             </div>
           </BodyContainer>
@@ -51,13 +96,13 @@ const BodyGrid = styled.div`
   display: grid;
   grid-template-rows: 75px 1fr;
   grid-template-columns: 300px 1fr;
-  
+
   @media screen and (max-width: 600px) {
     display: flex;
     flex-direction: column;
     height: inherit;
   }
-`
+`;
 
 const BodyContainer = styled.div`
   grid-column: 2 / 3;
@@ -69,69 +114,69 @@ const BodyContainer = styled.div`
   @media screen and (max-width: 600px) {
     order: 2;
   }
-  
+
   & > div {
     max-width: ${props => props.theme.contentWidthLaptop};
     margin: auto;
   }
-  
+
   & > h1 {
     color: ${props => props.theme.accentDark};
   }
-`
+`;
 
 const HeaderContainer = styled.div`
   grid-column: 1 / 3;
   grid-row: 1 / 2;
   z-index: 2;
-   @media screen and (max-width: 600px) {
+  @media screen and (max-width: 600px) {
     order: 1;
   }
-`
+`;
 
 const ToCContainer = styled.div`
   grid-column: 1 / 2;
   grid-row: 2 / 3;
   background: ${props => props.theme.lightGrey};
   overflow: scroll;
-   @media screen and (max-width: 600px) {
+  @media screen and (max-width: 600px) {
     order: 3;
     overflow: inherit;
   }
-`
+`;
 
 /* eslint no-undef: "off"*/
 export const pageQuery = graphql`
   query LessonBySlug($slug: String!) {
-    allPostTitles: allMarkdownRemark{
-        edges {
-          node {
-            frontmatter {
-              title
-              lesson
-              chapter
-              type
-            }
-            fields {
-              slug
-            }
+    allPostTitles: allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            title
+            lesson
+            chapter
+            type
+          }
+          fields {
+            slug
           }
         }
       }
-      postBySlug: markdownRemark(fields: { slug: { eq: $slug } }) {
-        html
-        timeToRead
-        excerpt
-        frontmatter {
-          title
-          cover
-          date
-          category
-          tags
-        }
-        fields {
-          slug
-        }
-      } 
+    }
+    postBySlug: markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      timeToRead
+      excerpt
+      frontmatter {
+        title
+        cover
+        date
+        category
+        tags
+      }
+      fields {
+        slug
+      }
+    }
   }
 `;
